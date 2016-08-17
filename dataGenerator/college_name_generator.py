80377e38597a880290f631446bf74e9ae9e810f0
Muhammad Ali Gulzar <gulzarmuhammadali@gmail.com>
2016-08-17 00:47:57 -0400

    year in college is reassigned

--- dataGenerator/college_name_generator.py
+++ dataGenerator/college_name_generator.py
@@ -15,8 +15,14 @@ for a in range(1,results.num):
 	name  = names.get_full_name()
 	month = str(random.randint(1, 12))
 	day = str(random.randint(1, 30))
-	year = str(random.randint(1988, 2000))
 	year_College = str(random.randint(1,4))
+	def switch(x):
+ 	   return {
+    	    '1': str(random.randint(1997, 2000)),
+        	'2': str(random.randint(1994, 1997)),
+        	'3': str(random.randint(1992, 1994)),
+        	'4': str(random.randint(1988, 1992)),}[x]
+	year = switch(year_College)
 	phone_number = str(random.randint(2000000000, 9999999999))
 	line = name + " " + year_College+" "+  month+"/" + day+ "/" + year + " " + phone_number + "\n"
 	f.write(line)
